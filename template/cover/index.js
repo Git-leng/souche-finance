3139bc81501db439ed0fa721a4b66b6d3afafbe3
lengbowen <lengbowen@souche.com>
2020-04-09 18:57:04 +0800

    feat: 微信模板

--- template/cover/index.js
+++ template/cover/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 import Util from '../util.js';
-let domain = process.env.<%- $$.convertUrl(data.project.domain) || 'HOST' %>;
-let instance = Util.request(domain || '');
+let instance = Util.request();
 
 <% _.forEach(data.list, function(o){ %> /** <%- o.summary %> */
 export function <%- $$.convertUrl(o.path) %><T = any> (opts?: any) {
     let host = Util.getHost('<%- data.project.domain %>');
+
     return instance<T>({
         method: '<%- o.method %>',
         url: host + '<%- o.path %>',
