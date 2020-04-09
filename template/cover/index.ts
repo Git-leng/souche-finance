3ef3f0e9c0596a0c8c11e89f5ddae67770b27473
lengbowen <lengbowen@souche.com>
2020-04-09 18:51:46 +0800

    feat: 微信api

--- template/cover/index.ts
+++ template/cover/index.ts
@@ -1,13 +1,14 @@
 /* eslint-disable */
-import Util from '../util';
+import Util from '../util.js';
 let domain = process.env.<%- $$.convertUrl(data.project.domain) || 'HOST' %>;
 let instance = Util.request(domain || '');
 
 <% _.forEach(data.list, function(o){ %> /** <%- o.summary %> */
 export function <%- $$.convertUrl(o.path) %><T = any> (opts?: any) {
+    let host = Util.getHost('<%- data.project.domain %>');
     return instance<T>({
         method: '<%- o.method %>',
-        url: '<%- o.path %>',
+        url: host + '<%- o.path %>',
         opts: opts
     });
 }
