69156ff619af69fae10c4657b82610fe69f3d831
lengbowen <lengbowen@souche.com>
2020-04-10 14:25:49 +0800

    feat: 接口自动化

--- template/cover/index.js
+++ template/cover/index.js
@@ -3,8 +3,8 @@ import Util from '../util.js';
 let instance = Util.request('<%- data.project.domain %>');
 
 <% _.forEach(data.list, function(o){ %> /** <%- o.summary %> */
-export function <%- $$.convertUrl(o.path) %><T = any> (opts?: any) {
-    return instance<T>({
+export function <%- $$.convertUrl(o.path) %> (opts) {
+    return instance({
         method: '<%- o.method %>',
         url: '<%- o.path %>',
         opts: opts
