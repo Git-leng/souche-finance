7e1f98195c4fc199ef407cfb7c8e07bb4ef9f32c
冷博文 <lengbowen@souche.com>
2020-09-15 16:09:47 +0800

    2.x版本模板适配

--- template/cover/index.js
+++ template/cover/index.js
@@ -2,12 +2,11 @@
 import instance from './instance';
 
 <% _.forEach(data.list, function(o){ %> /** <%- o.summary %> */
-export function <%- $$.convertUrl(o.path) %> (opts) {
+export function <%- $$.convertUrl(`${o.path}/${o.method}`) %> (opts) {
     return instance({
         method: '<%- o.method %>',
         url: '<%- o.path %>',
         opts: opts,
-        res: <%- o.responseMessages[0].responseModel %>
     });
 }
 
