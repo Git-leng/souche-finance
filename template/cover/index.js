3a9f17d469e0b53122f0979fe14775841f2fffce
冷博文 <lengbowen@souche.com>
2020-09-16 10:29:46 +0800

    fn方法名调整

--- template/cover/index.js
+++ template/cover/index.js
@@ -5,7 +5,7 @@ let domain = process.env.<%- data.project.domain || 'DOMAIN' %>;
 let instance = Util.finance(domain);
 
 <% _.forEach(data.list, function(o){ %> /** <%- o.summary %> */
-export function <%- $$.convertUrl(`${o.path}/${o.method}`) %> (opts) {
+export function <%- $$.convertUrl(`${o.path}/${o.method}`.replace(/-/g, '_')) %> (opts) {
     return instance({
         method: '<%- o.method %>',
         url: '<%- o.path %>',
